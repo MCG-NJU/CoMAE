3fcf391d4af57584b9cc506b15c7801ca60943ce
Jiange Yang (FA Talent) <v-jiangeyang@microsoft.com>
2023-03-19 18:35:40 +0800

    commit

---
+++ models_cpc.py
@@ -0,0 +1,134 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# timm: https://github.com/rwightman/pytorch-image-models/tree/master/timm
+# DeiT: https://github.com/facebookresearch/deit
+# --------------------------------------------------------
+from functools import partial
+
+import torch
+import torch.nn as nn
+
+from timm.models.vision_transformer import PatchEmbed, Block
+
+
+import numpy as np
+
+
+class MaskedAutoencoderViT(nn.Module):
+
+    def __init__(self, img_size=224, patch_size=16, in_chans=3,
+                 embed_dim=1024, depth=24, num_heads=16,
+                 decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        super().__init__()
+
+        # --------------------------------------------------------------------------
+        # encoder
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        self.patch_embed2 = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(depth)])
+
+        self.norm_pix_loss = norm_pix_loss
+
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        self.loss_img = nn.CrossEntropyLoss()
+        self.loss_hha = nn.CrossEntropyLoss()
+
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        w2 = self.patch_embed2.proj.weight.data
+        torch.nn.init.xavier_uniform_(w2.view([w2.shape[0], -1]))
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+
+    def forward(self, imgs, hha, mask_ratio=0.75):
+
+        hha = self.patch_embed2(hha)
+        imgs = self.patch_embed(imgs)
+
+
+        for blk in self.blocks:
+            hha = blk(hha)
+            imgs = blk(imgs)
+
+        # normalized features
+        imgs = imgs / imgs.norm(dim=-1, keepdim=True)
+        hha = hha / hha.norm(dim=-1, keepdim=True)
+        loss_all = 0
+        for i in range(0, imgs.size(0)):
+            logit_scale = self.logit_scale.exp()
+            logits_per_image = logit_scale * imgs[i] @ hha[i].t()
+            logits_per_hha = logits_per_image.t()
+            labels = torch.arange(imgs.size(1), dtype=torch.long).cuda()
+            loss_i = self.loss_img(logits_per_image, labels)
+            loss_h = self.loss_hha(logits_per_hha, labels)
+            loss = (loss_i + loss_h) / 2
+            loss_all = loss_all + loss
+        loss_avg = loss_all / imgs.size(0)
+
+        return loss_avg
+
+
+def mae_vit_small_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=384, depth=12, num_heads=12,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def mae_vit_base_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def mae_vit_large_patch16_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=16, embed_dim=1024, depth=24, num_heads=16,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def mae_vit_huge_patch14_dec512d8b(**kwargs):
+    model = MaskedAutoencoderViT(
+        patch_size=14, embed_dim=1280, depth=32, num_heads=16,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+# set recommended archs
+mae_vit_small_patch16 = mae_vit_small_patch16_dec512d8b
+mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+mae_vit_large_patch16 = mae_vit_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+mae_vit_huge_patch14 = mae_vit_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
